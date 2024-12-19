6c6a839f71d29b6331eeb27c536f6b82427b6089
Carlos Peres <chsilvaperes@gmail.com>
2024-12-19 20:57:02 -0300

    .

--- Inicio/inicio.js
+++ Inicio/inicio.js
@@ -1,16 +1,17 @@
-let count = 1;
-document.getElementById("radio1").checked = true;
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+document.getElementById('radio1').checked = true 
 
-setInterval(function(){
-    nextImage();
-}, 2000)
+setInterval(() => {
+    proximg()
+}, 5000)
 
-function nextImage(){
-    count++;
-    if(count>4){
-        count = 1
+function proximg(){
+    cont++
+
+    if(cont > 4){
+        cont = 1
     }
 
-    document.getElementById("radio"+count).checked = true;
+    document.getElementById('radio'+cont).checked = true 
 }
-
