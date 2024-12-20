1125e66891704d9483b9d7e179e6da5c71a42e27
Carlos Peres <chsilvaperes@gmail.com>
2024-12-19 22:45:02 -0300

    Correção Sliders

--- Inicio/inicio.js
+++ Inicio/inicio.js
@@ -1,17 +1,30 @@
-var radio = document.querySelector('.manual-btn')
-var cont = 1
-document.getElementById('radio1').checked = true 
+let cont1 = 1; 
+document.getElementById("slider1-radio1").checked = true;
 
-setInterval(() => {
-    proximg()
+setInterval(function() {
+    nextImage1();
 }, 5000)
 
-function proximg(){
-    cont++
-
-    if(cont > 4){
-        cont = 1
+function nextImage1(){
+    cont1++;
+    if(cont1 > 4) {
+        cont1 = 1;
     }
 
-    document.getElementById('radio'+cont).checked = true 
+    document.getElementById("slider1-radio"+cont1).checked = true;
+}
+let cont2 = 1; 
+document.getElementById("slider2-radio1").checked = true;
+
+setInterval(function() {
+    nextImage2();
+}, 5000)
+
+function nextImage2(){
+    cont2++;
+    if(cont2>4){
+        cont2 = 1;
+    }
+
+    document.getElementById("slider2-radio"+cont2).checked = true;
 }
